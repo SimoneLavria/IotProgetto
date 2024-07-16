a46d25f32bcb84c26d04bbb16b7c1b03433cafaa
SimoneLavria <105886997+SimoneLavria@users.noreply.github.com>
2024-07-16 17:21:49 +0200

    From All to -1

--- JavaBle/app/src/main/java/com/example/javable/PageTwoActivity.java
+++ JavaBle/app/src/main/java/com/example/javable/PageTwoActivity.java
@@ -218,7 +218,7 @@ public class PageTwoActivity extends AppCompatActivity {
                         savePublicly(s_primo);
                     }
                     String stringa_valore=messaggio.getText().toString();
-                    if(stringa_valore.equals("ALL"))
+                    if(stringa_valore.equals("-1"))
                     {
                         messaggio.setText(s);
                         String s_primo=s+'\n';
